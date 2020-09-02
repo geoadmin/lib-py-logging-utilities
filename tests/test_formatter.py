ba78e0f9a93722595366c651afb304fd7198be18
Brice Schaffner <brice.schaffner@swisstopo.ch>
2020-09-02 16:35:37 +0200

    Fixed Travis build
    Removed the version from dev_requirements.txt in order to let travis
    build with older python version. Also uses OrderedDict for older python
    version in unittests otherwise the json oject key order is not kept and
    the comparaison is then not possible.
    The test for the log.exception() had the path to the test environment
    which differ between test environment. Therefore now only test the start
    and end of the backtrace equality.
    
    Don't uses the "make test" for running test but call directly nose2 command.
    The make target creates a python virtualenv which don't work as expected
    in Travis as it already uses virtualenv.

--- tests/test_formatter.py
+++ tests/test_formatter.py
@@ -33,12 +33,12 @@ class BasicJsonFormatterTest(unittest.TestCase):
             const_attribute = ConstAttribute(application="test_application", empty_attr='')
             handler.addFilter(const_attribute)
             formatter = JsonFormatter(
-                dictionary(
-                    [
-                        ('level', 'levelname'), ('app', 'application'),
-                        ('empty_attr', 'empty_attr'), ('message', 'message')
-                    ]
-                ),
+                dictionary([
+                    ('level', 'levelname'),
+                    ('app', 'application'),
+                    ('empty_attr', 'empty_attr'),
+                    ('message', 'message'),
+                ]),
                 remove_empty=remove_empty,
                 filter_attributes=['application', 'empty_attr']
             )
@@ -51,29 +51,32 @@ class BasicJsonFormatterTest(unittest.TestCase):
             logger.info('Simple message')
             logger.info('Composed message: %s', 'this is a composed message')
             logger.info('Composed message %s', 'with extra', extra={'extra1': 23})
-        self.assertEqual(
-            ctx.output,
-            [
-                json.dumps(
-                    {
-                        "levelname": "INFO", "name": "test_formatter", "message": "Simple message"
-                    }
-                ),
-                json.dumps(
-                    {
-                        "levelname": "INFO",
-                        "name": "test_formatter",
-                        "message": "Composed message: this is a composed message"
-                    }
-                ),
-                json.dumps(
-                    {
-                        "levelname": "INFO",
-                        "name": "test_formatter",
-                        "message": "Composed message with extra"
-                    }
-                )
-            ]
+        message1 = json.loads(ctx.output[0], object_pairs_hook=dictionary)
+        message2 = json.loads(ctx.output[1], object_pairs_hook=dictionary)
+        message3 = json.loads(ctx.output[2], object_pairs_hook=dictionary)
+        self.assertDictEqual(
+            message1,
+            dictionary([
+                ("levelname", "INFO"),
+                ("name", "test_formatter"),
+                ("message", "Simple message"),
+            ])
+        )
+        self.assertDictEqual(
+            message2,
+            dictionary([
+                ("levelname", "INFO"),
+                ("name", "test_formatter"),
+                ("message", "Composed message: this is a composed message"),
+            ])
+        )
+        self.assertDictEqual(
+            message3,
+            dictionary([
+                ("levelname", "INFO"),
+                ("name", "test_formatter"),
+                ("message", "Composed message with extra"),
+            ])
         )
 
     def test_embedded_object(self):
@@ -81,60 +84,67 @@ class BasicJsonFormatterTest(unittest.TestCase):
             logger = logging.getLogger('test_formatter')
             self._configure_logger(
                 logger,
-                fmt=dictionary(
-                    [
-                        ('levelname', 'levelname'), ('msg', 'message'),
-                        ('system', {
-                            'context': {
-                                'module': 'module', 'file': 'filename'
-                            }
-                        })
-                    ]
-                )
+                fmt=dictionary([
+                    ('levelname', 'levelname'),
+                    ('msg', 'message'),
+                    (
+                        'system',
+                        dictionary([
+                            ('context', dictionary([
+                                ('module', 'module'),
+                                ('file', 'filename'),
+                            ]))
+                        ])
+                    ),
+                ])
             )
             logger.info('Simple message')
             logger.info('Composed message: %s', 'this is a composed message')
             logger.info('Composed message %s', 'with extra', extra={'extra1': 23})
-        self.assertEqual(
-            ctx.output,
-            [
-                json.dumps(
-                    {
-                        "levelname": "INFO",
-                        "msg": "Simple message",
-                        "system":
-                            {
-                                "context": {
-                                    "module": "test_formatter", "file": "test_formatter.py"
-                                }
-                            }
-                    }
+        message1 = json.loads(ctx.output[0], object_pairs_hook=dictionary)
+        message2 = json.loads(ctx.output[1], object_pairs_hook=dictionary)
+        message3 = json.loads(ctx.output[2], object_pairs_hook=dictionary)
+        self.assertDictEqual(
+            message1,
+            dictionary([
+                ("levelname", "INFO"),
+                ("msg", "Simple message"),
+                (
+                    "system",
+                    dictionary([(
+                        "context",
+                        dictionary([("module", "test_formatter"), ("file", "test_formatter.py")])
+                    )])
                 ),
-                json.dumps(
-                    {
-                        "levelname": "INFO",
-                        "msg": "Composed message: this is a composed message",
-                        "system":
-                            {
-                                "context": {
-                                    "module": "test_formatter", "file": "test_formatter.py"
-                                }
-                            }
-                    }
+            ])
+        )
+        self.assertDictEqual(
+            message2,
+            dictionary([
+                ("levelname", "INFO"),
+                ("msg", "Composed message: this is a composed message"),
+                (
+                    "system",
+                    dictionary([(
+                        "context",
+                        dictionary([("module", "test_formatter"), ("file", "test_formatter.py")])
+                    )])
                 ),
-                json.dumps(
-                    {
-                        "levelname": "INFO",
-                        "msg": "Composed message with extra",
-                        "system":
-                            {
-                                "context": {
-                                    "module": "test_formatter", "file": "test_formatter.py"
-                                }
-                            }
-                    }
-                )
-            ]
+            ])
+        )
+        self.assertDictEqual(
+            message3,
+            dictionary([
+                ("levelname", "INFO"),
+                ("msg", "Composed message with extra"),
+                (
+                    "system",
+                    dictionary([(
+                        "context",
+                        dictionary([("module", "test_formatter"), ("file", "test_formatter.py")])
+                    )])
+                ),
+            ])
         )
 
     def test_embedded_list(self):
@@ -142,36 +152,32 @@ class BasicJsonFormatterTest(unittest.TestCase):
             logger = logging.getLogger('test_formatter')
             self._configure_logger(
                 logger,
-                fmt=OrderedDict(
-                    [
-                        ("levelname", "levelname"), ("name", "name"), ("message", "message"),
-                        (
-                            "list",
-                            [
-                                OrderedDict([("file", "filename")]),
-                                OrderedDict([("module", "module")])
-                            ]
-                        )
-                    ]
-                )
+                fmt=dictionary([
+                    ("levelname", "levelname"),
+                    ("name", "name"),
+                    ("message", "message"),
+                    (
+                        "list", [
+                            dictionary([("file", "filename")]),
+                            dictionary([("module", "module")]),
+                        ]
+                    ),
+                ])
             )
             logger.info('Composed message %s', 'with list')
-        self.assertEqual(
-            ctx.output,
-            [
-                json.dumps(
-                    {
-                        "levelname": "INFO",
-                        "name": "test_formatter",
-                        "message": "Composed message with list",
-                        'list': [{
-                            "file": "test_formatter.py"
-                        }, {
-                            "module": "test_formatter"
-                        }]
-                    }
-                )
-            ]
+        message = json.loads(ctx.output[0], object_pairs_hook=dictionary)
+        self.assertDictEqual(
+            message,
+            dictionary([
+                ("levelname", "INFO"),
+                ("name", "test_formatter"),
+                ("message", "Composed message with list"),
+                ('list', [{
+                    "file": "test_formatter.py"
+                }, {
+                    "module": "test_formatter"
+                }]),
+            ])
         )
 
     def test_extra_list(self):
@@ -187,22 +193,19 @@ class BasicJsonFormatterTest(unittest.TestCase):
                     "extra-2": "module"
                 }]}
             )
-        self.assertEqual(
-            ctx.output,
-            [
-                json.dumps(
-                    {
-                        "levelname": "INFO",
-                        "name": "test_formatter",
-                        "message": "Composed message with extra",
-                        'extra-list': [{
-                            "extra-1": "file"
-                        }, {
-                            "extra-2": "module"
-                        }]
-                    }
-                )
-            ]
+        message = json.loads(ctx.output[0], object_pairs_hook=dictionary)
+        self.assertDictEqual(
+            message,
+            dictionary([
+                ("levelname", "INFO"),
+                ("name", "test_formatter"),
+                ("message", "Composed message with extra"),
+                ('extra-list', [{
+                    "extra-1": "file"
+                }, {
+                    "extra-2": "module"
+                }]),
+            ])
         )
 
     def test_exception(self):
@@ -213,25 +216,24 @@ class BasicJsonFormatterTest(unittest.TestCase):
                 raise ValueError("My value error")
             except ValueError as err:
                 logger.exception('Exception message: %s', err)
-        self.assertDictEqual(
-            json.loads(ctx.output[0], object_pairs_hook=dictionary),
-            dictionary(
-                [
-                    ("levelname", "ERROR"),
-                    ("name", "test_formatter"),
-                    ("message", "Exception message: My value error"),
-                    (
-                        "exc_text",
-                        'Traceback (most recent call last):\n'
-                        # pylint: disable=line-too-long
-                        '  File "/home/ltshb/repo-lib/lib-py-log-json-formatter/tests/test_formatter.py", '
-                        'line 213, in test_exception\n'
-                        '    raise ValueError("My value error")\n'
-                        'ValueError: My value error'
-                    )
-                ]
-            )
-        )
+        message = json.loads(ctx.output[0], object_pairs_hook=dictionary)
+        expected_msg = dictionary([
+            ("levelname", "ERROR"),
+            ("name", "test_formatter"),
+            ("message", "Exception message: My value error"),
+            (
+                "exc_text",
+                'Traceback (most recent call last):\n'
+                '    raise ValueError("My value error")\n'
+                'ValueError: My value error'
+            ),
+        ])
+        self.assertEqual(message.keys(), expected_msg.keys())
+        self.assertEqual(message['levelname'], expected_msg['levelname'])
+        self.assertEqual(message['name'], expected_msg['name'])
+        self.assertEqual(message['message'], expected_msg['message'])
+        self.assertEqual(message['exc_text'][:36], expected_msg['exc_text'][:36])
+        self.assertEqual(message['exc_text'][-66:], expected_msg['exc_text'][-66:])
 
     def test_remove_empty(self):
         with self.assertLogs('test_formatter', level=logging.DEBUG) as ctx:
@@ -240,12 +242,11 @@ class BasicJsonFormatterTest(unittest.TestCase):
             logger.info('Composed message %s', 'remove empty')
         self.assertDictEqual(
             json.loads(ctx.output[0], object_pairs_hook=dictionary),
-            dictionary(
-                [
-                    ("level", "INFO"), ("app", "test_application"),
-                    ("message", "Composed message remove empty")
-                ]
-            )
+            dictionary([
+                ("level", "INFO"),
+                ("app", "test_application"),
+                ("message", "Composed message remove empty"),
+            ])
         )
 
     def test_leave_empty(self):
@@ -255,10 +256,10 @@ class BasicJsonFormatterTest(unittest.TestCase):
             logger.info('Composed message %s', 'leave empty')
         self.assertDictEqual(
             json.loads(ctx.output[0], object_pairs_hook=dictionary),
-            dictionary(
-                [
-                    ("level", "INFO"), ("app", "test_application"), ('empty_attr', ''),
-                    ("message", "Composed message leave empty")
-                ]
-            )
+            dictionary([
+                ("level", "INFO"),
+                ("app", "test_application"),
+                ('empty_attr', ''),
+                ("message", "Composed message leave empty"),
+            ])
         )
