bfc9b08f28781fc963ac5f2554052858ccee3e7a
Brice Schaffner <brice.schaffner@swisstopo.ch>
2022-10-18 08:15:35 +0200

    Added auto versioning

--- logging_utilities/__init__.py
+++
@@ -1,6 +0,0 @@
-VERSION = (3, 1, 0)
-if isinstance(VERSION[-1], str):
-    # Support for alpha version: 0.1.0-alpha1
-    __version__ = "-".join([".".join(map(str, VERSION[:-1])), VERSION[-1]])  # pragma: no cover
-else:
-    __version__ = ".".join(map(str, VERSION))
